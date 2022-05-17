eb590acf9c4bcfe708dd5b1b711910e701cc9cb0
philjacks <philjacks.webdev@gmail.com>
2022-05-17 10:33:17 +0100

    endpoint added for updating article votes

--- models/articlesModels.js
+++ models/articlesModels.js
@@ -1,18 +1,32 @@
 const db = require("../db/connection");
 
 exports.fetchArticleByIdFromDb = (id) => {
-  console.log(id);
   const queryStr = `
         SELECT * FROM articles
         WHERE article_id = $1
     `;
-  return db
-    .query(queryStr, [id])
-    .then((data) => {
-      const article = data.rows[0];
-      if (!article) {
-        return Promise.reject({ status: 404, msg: "Not found" });
-      }
-      return article;
-    })
+  return db.query(queryStr, [id]).then((data) => {
+    const article = data.rows[0];
+    if (!article) {
+      return Promise.reject({ status: 404, msg: "Not found" });
+    }
+    return article;
+  });
+};
+
+exports.updateArticleVotesInDb = (id, inc_votes) => {
+  const queryStr = `
+    UPDATE articles
+    SET votes = votes + $2
+    WHERE article_id = $1
+    RETURNING *
+  `;
+
+  return db.query(queryStr, [id, inc_votes]).then((data) => {
+    const article = data.rows[0];
+    if (!article) {
+      return Promise.reject({ status: 404, msg: "Not found" });
+    }
+    return data.rows[0];
+  });
 };
