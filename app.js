91e133149f18e423b96ef47a31aeb7985768f686
philjacks <philjacks.webdev@gmail.com>
2022-05-17 16:10:40 +0100

    get all articles endpoint added with comment count

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ const { getUsers } = require("./controllers/usersControllers");
 const {
   getArticleById,
   updateArticleVotes,
+  getArticles,
 } = require("./controllers/articlesControllers");
 
 const app = express();
 
 app.use(express.json());
 
+// Topics
 app.get("/api/topics", getTopics);
+
+// Articles
+app.get("/api/articles", getArticles);
 app.get("/api/articles/:article_id", getArticleById);
 app.patch("/api/articles/:article_id", updateArticleVotes);
+
+// Users
 app.get("/api/users", getUsers);
 
 app.use((err, req, res, next) => {
@@ -29,6 +36,11 @@ app.use((err, req, res, next) => {
   } else {
     next(err);
   }
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: err.msg });
 });
 
 module.exports = app;
