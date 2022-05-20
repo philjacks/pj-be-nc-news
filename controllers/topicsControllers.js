73650eac45b12d711bed8b6c124191db05e9b918
philjacks <philjacks.webdev@gmail.com>
2022-05-20 14:29:59 +0100

    POST /api/topics added for creating new topic

--- controllers/topicsControllers.js
+++ controllers/topicsControllers.js
@@ -1,9 +1,24 @@
-const { fetchTopicsFromDb } = require("../models/topicsModels");
+const {
+  fetchTopicsFromDb,
+  postNewTopicToDb,
+} = require("../models/topicsModels");
 
 exports.getTopics = (req, res, next) => {
   fetchTopicsFromDb()
     .then((topics) => {
       res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.postNewTopic = (req, res, next) => {
+  const newTopic = req.body;
+
+  postNewTopicToDb(newTopic)
+    .then((topic) => {
+      res.status(201).send({ topic });
     })
     .catch((err) => {
       next(err);
--- models/topicsModels.js
+++ models/topicsModels.js
@@ -5,7 +5,28 @@ exports.fetchTopicsFromDb = () => {
     SELECT * FROM topics
     `;
   return db.query(qeuryStr).then((data) => {
-    
     return data.rows;
+  });
+};
+
+exports.postNewTopicToDb = (newTopic) => {
+  const { slug, description } = newTopic;
+
+  
+
+  if (typeof slug !== "string" || typeof description !== "string") {
+    return Promise.reject({ status: 400, msg: "Bad request" });
+  }
+
+  const qeuryStr = `
+    INSERT INTO topics
+    (slug, description)
+    VALUES 
+    ($1, $2)
+    RETURNING *
+  `;
+
+  return db.query(qeuryStr, [slug, description]).then((data) => {
+    return data.rows[0];
   });
 };
